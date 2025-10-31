b88295ad80189e1216e772992f7407c90460d4da
Axel Vega <Al07167544@tecmilenio.mx>
2025-10-31 11:49:12 -0600

    agregue donde la gente pone su nobre o cancela el registro y el menu de selccion de actividades

--- SRC/Sistema_registro.py
+++ SRC/Sistema_registro.py
@@ -1,1 +1,26 @@
-num_personas=int(input("Cuantas"))
+num_personas = int(input("¿Cuántas personas se registaran?:"))
+cont_personas = 0
+cont_alberca = 0
+cont_tenis = 0
+cont_padel = 0
+
+ingresos_alberca = 0
+ingresos_tenis = 0
+ingresos_padel = 0
+ingreso_total = 0
+
+suma_pagos = 0
+resumen_clientes = ""
+
+while cont_personas < num_personas:
+    nombre = input("Nombre del socio (o escribe cancelar para salir):")
+    if nombre == "cancelar" or nombre == "Cancelar":
+        print("Registro cancelado.\n")
+        break
+
+    print("Que deporte realiza?")
+    print("1. Alberca")
+    print("2. Tenis")
+    print("3. Padel")
+    print("4. Mas de un deporte (Membresia completa)")
+    opcion = int(input("Elige una opcion (1-4): "))
