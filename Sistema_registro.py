02bb7abc124b001a9afa4a8039d5f7fc5787cb87
Pedro Antonio Rodriguez Grimaldo <pedroantoniorodriguezgrimaldo@gmail.com>
2025-11-25 12:19:01 -0600

    Implement loading animation and file saving
    Added loading animation and file saving functionality.

--- Sistema_registro.py
+++ Sistema_registro.py
@@ -1,80 +1,24 @@
-num_personas = int(input("¿Cuántas personas se registaran?:"))
-cont_personas = 0
-cont_alberca = 0
-cont_tenis = 0
-cont_padel = 0
+#   SISTEMA DE REGISTRO - CLUB BRITANIA
 
-ingresos_alberca = 0
-ingresos_tenis = 0
-ingresos_padel = 0
-ingreso_total = 0
+import time
 
-suma_pagos = 0
-resumen_clientes = ""
-
-while cont_personas < num_personas:
-    nombre = input("Nombre del socio (o escribe cancelar para salir):")
-    if nombre == "cancelar" or nombre == "Cancelar":
-        print("Registro cancelado.\n")
-        break
-    print("Que deporte realiza?")
-    print("1. Alberca")
-    print("2. Tenis")
-    print("3. Padel")
-    print("4. Mas de un deporte (Membresia completa)")
-    opcion = int(input("Elige una opcion (1-4): "))
-
-    if opcion == 1:
-        pago = 1800
-        cont_alberca += 1
-        ingresos_alberca += pago
-        deportes = "Alberca"
-    elif opcion == 2:
-        pago = 2350
-        cont_tenis += 1
-        ingresos_tenis += pago
-        deportes = "Tenis"
-    elif opcion == 3:
-        pago = 2400
-        cont_padel += 1
-        ingresos_padel += pago
-        deportes = "Padel"
-    elif opcion == 4:
-        pago = 3000
-        deportes = "Alberca, Tenis y Padel"
-    else:
-        print("Opcion no valida, inteta de nuevo. \n")
-        continue
-
-    cont_personas += 1
-    ingreso_total += pago
-    suma_pagos += pago
-    resumen_clientes += "Nombre: " + nombre + " | Deportes: " + deportes + " | Pago: $" + str(pago) + "\n"
-
-print()
-print("REPORTE FINAL - CLUB BRITANIA")
-print("\n")
-print("Total de socios registrados:", cont_personas)
-print("\nresumen de clientes:\n")
-
-print("\nResumen de clientes:\n")
-print(resumen_clientes)
-
-print()
-print("Total por deporte:")
-print("Alberca:", cont_alberca, "personas | Ingreso: $", ingresos_alberca)
-print("Tenis:", cont_tenis, "personas | Ingreso: $", ingresos_tenis)
-print("Padel:", cont_padel, "personas | Ingreso: $", ingresos_padel)
-
-print()
-print("Ganancia total: $", ingreso_total)
-
-if cont_personas > 0:
-    print("Promedio de ganancia por persona: $", ingreso_total / cont_personas)
-
-print("\nRegistro finalizado con éxito.")
+def mostrar_carga():
+    print("CARGANDO PROGRAMA")
+    for i in range(10): #Esta linea de codigo indica cuantas veces se va a imprimir "."
+        print(".", end="", flush=True) #El print "." puede ser modificado, por ejemplo le puedes poner "-" y se va a imprimir una linea horizontal del mismo
+        time.sleep(0.3) #Esta funcion es cuando el programa se va a apagar y encender
+    print(" Listo!\n")
 
 
+mostrar_carga()
 
+# Función: guardar los cliente en los archivos
 
-
+def guardar_en_archivo(cliente):
+    try:
+        with open("registro_britania.txt", "a") as archivo:
+            archivo.write("Nombre: " + cliente["nombre"] +
+                          " | Deporte: " + cliente["deporte"] +
+                          " | Pago: $" + str(cliente["pago"]) + "\n")
+    except:
+        print("Error al guardar en archivo.")
