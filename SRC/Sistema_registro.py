212f996b4d85dc85e6f9a990c9c48796103c2f18
Axelvega25 <AL07167544@tecmilenio.mx>
2025-11-28 20:33:21 -0600

    Update Sistema_registro.py
    Avance del proyecto

--- SRC/Sistema_registro.py
+++ SRC/Sistema_registro.py
@@ -17,10 +17,59 @@ while cont_personas < num_personas:
     if nombre == "cancelar" or nombre == "Cancelar":
         print("Registro cancelado.\n")
         break
-
     print("Que deporte realiza?")
     print("1. Alberca")
     print("2. Tenis")
     print("3. Padel")
     print("4. Mas de un deporte (Membresia completa)")
     opcion = int(input("Elige una opcion (1-4): "))
+
+    if opcion == 1:
+        pago = 1800
+        cont_alberca += 1
+        ingresos_alberca += pago
+        deportes = "Alberca"
+    elif opcion == 2:
+        pago = 2350
+        cont_tenis += 1
+        ingresos_tenis += pago
+        deportes = "Tenis"
+    elif opcion == 3:
+        pago = 2400
+        cont_padel += 1
+        ingresos_padel += pago
+        deportes = "Padel"
+    elif opcion == 4:
+        pago = 3000
+        deportes = "Alberca, Tenis y Padel"
+    else:
+        print("Opcion no valida, inteta de nuevo. \n")
+        continue
+
+    cont_personas += 1
+    ingreso_total += pago
+    suma_pagos += pago
+    resumen_clientes += "Nombre: " + nombre + " | Deportes: " + deportes + " | Pago: $" + str(pago) + "\n"
+
+print()
+print("REPORTE FINAL - CLUB BRITANIA")
+print("\n")
+print("Total de socios registrados:", cont_personas)
+print("\nresumen de clientes:\n")
+
+print("\nResumen de clientes:\n")
+print(resumen_clientes)
+
+print()
+print("Total por deporte:")
+print("Alberca:", cont_alberca, "personas | Ingreso: $", ingresos_alberca)
+print("Tenis:", cont_tenis, "personas | Ingreso: $", ingresos_tenis)
+print("Padel:", cont_padel, "personas | Ingreso: $", ingresos_padel)
+
+print()
+print("Ganancia total: $", ingreso_total)
+
+if cont_personas > 0:
+    print("Promedio de ganancia por persona: $", ingreso_total / cont_personas)
+
+print("\nRegistro finalizado con éxito.")
